f986aa0188a2fdbef2f889c2e5a3a6bf2fdcd72e
Jaime <jaimesof82@gmail.com>
2019-10-21 14:28:11 -0700

    added simple error handling

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
+
     res.status(200).json({
         message: 'Orders were fetched'
     });
+
 });
 
 router.post('/', (req, res, next) => {
--- app.js
+++ app.js
@@ -1,13 +1,33 @@
 const express = require('express');
 const app = express();
-const router = express.Router();
+// morgan is a package that logs request info (type of req, status, ms, etc) to the terminal console with each req
+const morgan = require('morgan');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+
+app.use(morgan('dev'));
 
 // use() will set up middleware
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
+// if the code makes it to here, that means that the route was not found and a 404 error will be thrown
+app.use((req, res, next) => {
+    const error = new Error('Not Found');
+    error.status = 404;
+    next(error);
+});
+
+// this will handle any errors besides 404 not found 
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+
+    })
+});
 
 module.exports = app;
